7d9f0e50cbdfb51f07cbcc48fe9b4f90f7828531
Nikki <nikkibuqu@gmail.com>
2025-03-26 01:47:56 -0400

    A few hardcoded vibes just to get things going

---
+++ moodboard/vibe_engine.py
@@ -0,0 +1,5 @@
+import random
+from moodboard.vibes import VIBES
+
+def gen_vibe():
+    return random.choice(VIBES)
---
+++ moodboard/vibes.py
@@ -0,0 +1,47 @@
+VIBES = [
+        {
+            "mood": "Lavender Doom",
+            "quote": "Softness is not weakness—it's rebellion in velvet.",
+            "song": "https://open.spotify.com/track/1EXxMtYkl3fYbB1VFUsgEk",
+            "color": "#D6B2FF",
+            "suggestion": "Light a candle and journal your villain origin story."
+
+            },
+        {
+            "mood": "Sunlight in Reverse",
+            "quote": "The sky remembers what we forget.",
+            "song": "https://open.spotify.com/track/7o2CTH4ctstm8TNelqjb51",
+            "color": "#FFD6A5",
+            "suggestion": "Take a photo of something glowing and send it to nobody."
+
+            },
+        {
+            "mood": "Quiet Static",
+            "quote": "You don’t need to respond. You can just rest.",
+            "song": "https://open.spotify.com/track/2Z8WuEywRWYTKe1NybPQEW",
+            "color": "#A5B1C2",
+            "suggestion": "Turn off all notifications and let the silence stretch."
+
+            },
+        {
+            "mood": "Sadgirl",
+            "quote": "",
+            "song": "",
+            "color": "",
+            "suggestion": ""
+            },
+        {
+            "mood": "Simple Comforts",
+            "quote": "",
+            "song": "",
+            "color": "",
+            "suggestion": ""
+            },
+        {
+            "mood": "Witchmetal",
+            "quote": "",
+            "song": "",
+            "color": "",
+            "suggestion": ""
+            }
+        ]
